18212805b674f66735e5ec6d1a39c8d1f4ed0703
MicrocontrollersDev <microcontrollersyt@gmail.com>
2025-06-16 13:08:39 -0700

    port to 1.21.6

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,11 +2,12 @@ pluginManagement {
 	repositories {
 		mavenCentral()
 		gradlePluginPortal()
-		maven("https://maven.fabricmc.net/")
-		maven("https://maven.neoforged.net/releases/")
+		maven("https://maven.fabricmc.net")
+		maven("https://maven.neoforged.net/releases")
 		maven("https://maven.architectury.dev")
 		maven("https://maven.kikugie.dev/snapshots")
 		maven("https://maven.kikugie.dev/releases")
+		maven("https://repo.polyfrost.cc/releases")
 	}
 }
 
@@ -25,12 +26,11 @@ stonecutter {
 			}
 		}
 
-		mc("1.20.1", listOf("fabric"))
-		mc("1.20.6", listOf("fabric"))
 		mc("1.21.1", listOf("fabric"))
 		mc("1.21.3", listOf("fabric"))
+		mc("1.21.6", listOf("fabric"))
 
-		vcsVersion = "1.21.3-fabric"
+		vcsVersion = "1.21.6-fabric"
 	}
 }
 
